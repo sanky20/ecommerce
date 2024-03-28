9e7db8b89c14063f760773a19ec4f4652ef733f5
Sanskar Sharma <sankysaa20@gmail.com>
2024-03-28 18:18:45 +0530

    issue resolved

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import Order from "../models/OrderModel.js";
+import Order from "../models/orderModel.js";
 import Product from "../models/productModel.js";
 import { calcPrices } from "../utils/calcPrices.js";
 import { verifyPayPalPayment, checkIfNewTransaction } from "../utils/paypal.js";
